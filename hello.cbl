bc02967f8f98224f96588917ae9567a029ff17fe
Daniel Almeida <danz82158@gmail.com>
2020-04-19 11:54:52 -0300

    Comecando os Estudos de Cobol

---
+++ hello.cbl
@@ -0,0 +1,12 @@
+       Identification division.
+           program-id.hello.
+       procedure division.
+           display "######################################################".
+           display "Hello Rose, Welcome to the most exciting world of COBOL".
+
+           display "#################################################".
+           display "************************************************".
+           display "Author: Daniel Amarante de Almeida 18/04/2020".
+           display "********************************************".
+           
+           stop run.
